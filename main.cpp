debe824d8027f7af83d554949dcbeae9dce69d6a
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2026-03-19 12:40:46 -0600

    basics

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main() {
-    cout << 1.0 / 3.0 << endl;
+   vector<int> nums {1, 2, 3};
+   int total {};
+
+   nums.push_back(4);
+
+   for (const auto num : nums) {
+      total += num;
+   }
+
+   cout << total << endl;
 }
